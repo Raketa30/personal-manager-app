25ae436cd194f51b834afc7523461a8499a2dbe4
Igor Kulgachov <duckpool@yandex.ru>
2022-05-04 16:56:41 +0300

    initial commit

---
+++ src/main/java/ru/liga/application/PersonalManagerAppApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/liga/application/PersonalManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liga.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
